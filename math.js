efc3fca49d48c98b4df5e7bff0b56c25473e0f46
Jan Góralewski <jan.goralewski@o2.pl>
2022-10-17 03:27:48 +0200

    zadanie8

---
+++ math.js
@@ -0,0 +1,24 @@
+// kalulator js w Node
+
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mul(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+module.exports.add = add;
+module.exports.sub = sub;
+module.exports.mul = mul;
+module.exports.div = div;
+//zad.5 Rozbudowanie kalkulatora o stałą matematyczną PI i wypisanie tejże wartości na konsoli w naszej głównej aplikacji.
+// module.exports.Pi = 3.14;
